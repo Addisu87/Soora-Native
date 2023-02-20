455a714472c363d00562951a6196833845bfbb09
Addisu <addisuhaile87@gmail.com>
2023-02-20 18:28:53 +0300

    Update the navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Image, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 const Navbar = () => {
   return (
@@ -9,12 +9,21 @@ const Navbar = () => {
         <View>
           <Image
             source={require('../assets/images/logo.png')}
+            fadeDuration={0}
             className="w-24 h-24"
           />
         </View>
-        <View>
-          <Button title="Contacts" className="h-14 w-14" />
-          <Ionicons name="chevron-back-outline" size={34} color="FF5864" />
+        <View className="flex">
+          <Button
+            title="Contacts"
+            className="inline-flex justify-center rounded-2xl border border-transparent bg-zinc-900 px-16 py-3 md:py-2 md:px-8 text-base font-medium text-white shadow-sm hover:bg-zinc-700 focus:outline-none focus:ring-2 focus:ring-zinc-500 focus:ring-offset-2"
+          />
+          <AntDesign
+            name="arrowright"
+            size={24}
+            color="black"
+            className="rotate-45"
+          />
         </View>
       </View>
     </View>
